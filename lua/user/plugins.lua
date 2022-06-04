d84d1484155feb63d47aa01ee6d63201eeb98be9
Besher <beshoux@outlook.fr>
2022-06-04 17:53:37 +0200

    a

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -134,6 +134,9 @@ return packer.startup(function(use)
 	-- Color Highlighter
 	use("norcalli/nvim-colorizer.lua")
 
+	-- Debugging
+	use("mfussenegger/nvim-dap")
+
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then
